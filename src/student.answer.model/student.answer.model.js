30a05859aa19ef3e91e417a2a965b87f7157bffb
gpinula-2020433 <gpinula-2020433@kinal.edu.gt>
2025-08-27 16:58:47 -0600

    Se modifican modelos relacionados a cuestionarios

--- src/student.answer.model/student.answer.model.js
+++ src/student.answer.model/student.answer.model.js
@@ -2,28 +2,41 @@ import { Schema, model } from "mongoose"
 
 const studentAnswerSchema = Schema(
     {
-        studentEnrollmentId: {
+        studentCourseId: {
             type: Schema.Types.ObjectId,
-            ref: 'Enrollment',
-            required: [true, 'Student Enrollment is required']
+            ref: 'StudentCourse',
+            required: [true, 'Student Course is required']
+        },
+        questionnaireId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Questionnaire',
+            required: [true, 'Questionnaire is required']
         },
         questionId: {
             type: Schema.Types.ObjectId,
             ref: 'Question',
             required: [true, 'Question is required']
         },
+        type: {
+            type: String,
+            enum: ['CHOICE', 'OPEN'],
+            required: [true, 'Question type is required']
+        },
         selectedOptionId: {
             type: Schema.Types.ObjectId,
             ref: 'Option',
-            required: [true, 'Selected option is required']
+        },
+        answerText: {
+            type: String,
+            default: null
         },
         isCorrect: {
             type: Boolean,
-            required: [true, 'Correct answer flag is required']
+            default: null
         },
         pointsObtained: {
             type: Number,
-            required: [true, 'Points obtained are required']
+            default: 0
         }
     },
     {
